87707bf5716e0f7c2c0a1e2303f4d6ae2cfa45dc
Weverton Couto <wevertoncristais@gmail.com>
2022-12-15 19:49:17 -0300

    move production code to their own target

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:meta/meta.dart';
 
 import '../entities/entities.dart';
@@ -15,4 +17,23 @@ class AuthenticationParams {
     @required this.email,
     @required this.secret,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'email': email,
+      'password': secret,
+    };
+  }
+
+  factory AuthenticationParams.fromMap(Map<String, dynamic> map) {
+    return AuthenticationParams(
+      email: map['email'] as String,
+      secret: map['password'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AuthenticationParams.fromJson(String source) =>
+      AuthenticationParams.fromMap(json.decode(source) as Map<String, dynamic>);
 }
