74e01382b08d1c644cbdcb7c2c1d510487d91d4d
Weverton Couto <wevertoncristais@gmail.com>
2022-12-16 19:49:06 -0300

    ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- test/data/usecases/remove_authentication_test.dart
+++ test/data/usecases/remove_authentication_test.dart
@@ -90,4 +90,23 @@ void main() {
       throwsA(DomainError.unexpected),
     );
   });
+
+  test('Should throw InvalidCredentialsError if HttpClient returns 401',
+      () async {
+    // arrange
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenThrow(HttpError.unauthorized);
+
+    // act
+    final future = sut.auth(params);
+
+    //assert
+    expect(
+      future,
+      throwsA(DomainError.invalidCredentials),
+    );
+  });
 }
